8f9346316b0662e91b35d9650a7a282754d28889
Helloyunho <yunho050840@gmail.com>
2020-02-16 01:04:32 +0900

    Update resources and fix link

--- musl-cross.rb
+++ musl-cross.rb
@@ -1,9 +1,9 @@
 class MuslCross < Formula
   desc "Linux cross compilers based on musl libc"
-  homepage "https://github.com/Helloyunho/musl-cross-make"
-  url "https://github.com/Helloyunho/musl-cross-make/archive/v0.9.9.tar.gz"
+  homepage "https://github.com/richfelker/musl-cross-make"
+  url "https://github.com/richfelker/musl-cross-make/archive/v0.9.8.tar.gz"
   sha256 "886ac2169c569455862d19789a794a51d0fbb37209e6fae1bda7d6554a689aac"
-  head "https://github.com//Helloyunho/musl-cross-make.git"
+  head "https://github.com/richfelker/musl-cross-make.git"
 
   option "with-aarch64", "Build cross-compilers targeting arm-linux-muslaarch64"
   option "with-arm-hf", "Build cross-compilers targeting arm-linux-musleabihf"
@@ -21,44 +21,44 @@ class MuslCross < Formula
     sha256 "4ac22e4a619417213cfdab24714413bb9118fbaebe6012c6c89c279cdadef2ce"
   end
 
-  resource "mpfr-3.1.4.tar.bz2" do
-    url "https://ftp.gnu.org/gnu/mpfr/mpfr-3.1.4.tar.bz2"
-    sha256 "d3103a80cdad2407ed581f3618c4bed04e0c92d1cf771a65ead662cc397f7775"
+  resource "mpfr-4.0.2.tar.bz2" do
+    url "https://ftp.gnu.org/gnu/mpfr/mpfr-4.0.2.tar.bz2"
+    sha256 "c05e3f02d09e0e9019384cdd58e0f19c64e6db1fd6f5ecf77b4b1c61ca253acc"
   end
 
-  resource "mpc-1.0.3.tar.gz" do
-    url "https://ftp.gnu.org/gnu/mpc/mpc-1.0.3.tar.gz"
-    sha256 "617decc6ea09889fb08ede330917a00b16809b8db88c29c31bfbb49cbf88ecc3"
+  resource "mpc-1.1.0.tar.gz" do
+    url "https://ftp.gnu.org/gnu/mpc/mpc-1.1.0.tar.gz"
+    sha256 "6985c538143c1208dcb1ac42cedad6ff52e267b47e5f970183a3e75125b43c2e"
   end
 
-  resource "gmp-6.1.1.tar.bz2" do
-    url "https://ftp.gnu.org/gnu/gmp/gmp-6.1.1.tar.bz2"
-    sha256 "a8109865f2893f1373b0a8ed5ff7429de8db696fc451b1036bd7bdf95bbeffd6"
+  resource "gmp-6.2.0.tar.bz2" do
+    url "https://ftp.gnu.org/gnu/gmp/gmp-6.2.0.tar.bz2"
+    sha256 "f51c99cb114deb21a60075ffb494c1a210eb9d7cb729ed042ddb7de9534451ea"
   end
 
-  resource "musl-1.1.22.tar.gz" do
-    url "https://www.musl-libc.org/releases/musl-1.1.22.tar.gz"
-    sha256 "8b0941a48d2f980fd7036cfbd24aa1d414f03d9a0652ecbd5ec5c7ff1bee29e3"
+  resource "musl-1.1.24.tar.gz" do
+    url "https://www.musl-libc.org/releases/musl-1.1.24.tar.gz"
+    sha256 "1370c9a812b2cf2a7d92802510cca0058cc37e66a7bedd70051f0a34015022a3"
   end
 
-  resource "binutils-2.27.tar.bz2" do
-    url "https://ftp.gnu.org/gnu/binutils/binutils-2.27.tar.bz2"
-    sha256 "369737ce51587f92466041a97ab7d2358c6d9e1b6490b3940eb09fb0a9a6ac88"
+  resource "binutils-2.34.tar.bz2" do
+    url "https://ftp.gnu.org/gnu/binutils/binutils-2.34.tar.bz2"
+    sha256 "89f010078b6cf69c23c27897d686055ab89b198dddf819efb0a4f2c38a0b36e6"
   end
 
   resource "config.sub" do
-    url "https://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.sub;hb=3d5db9ebe860"
-    sha256 "75d5d255a2a273b6e651f82eecfabf6cbcd8eaeae70e86b417384c8f4a58d8d3"
+    url "https://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.sub;hb=5256817ace8493502ec88501a19e4051c2e220b0"
+    sha256 "f08fe8f207c0fa6d722312774c28365024682333f5547c8192d0547957b000af"
   end
 
-  resource "gcc-6.4.0.tar.xz" do
-    url "https://ftp.gnu.org/gnu/gcc/gcc-6.4.0/gcc-6.4.0.tar.xz"
-    sha256 "850bf21eafdfe5cd5f6827148184c08c4a0852a37ccf36ce69855334d2c914d4"
+  resource "gcc-9.2.0.tar.xz" do
+    url "https://ftp.gnu.org/gnu/gcc/gcc-9.2.0/gcc-9.2.0.tar.xz"
+    sha256 "ea6ef08f121239da5695f76c9b33637a118dcf63e24164422231917fa61fb206"
   end
 
-  resource "isl-0.15.tar.bz2" do
-    url "http://isl.gforge.inria.fr/isl-0.15.tar.bz2"
-    sha256 "8ceebbf4d9a81afa2b4449113cee4b7cb14a687d7a549a963deb5e2a41458b6b"
+  resource "isl-0.22.tar.bz2" do
+    url "http://isl.gforge.inria.fr/isl-0.22.tar.bz2"
+    sha256 "b21d354acd613a91cb88328753ec3aaeb174d6af042d89c5fcf3bbcced370751"
   end
 
   def install
